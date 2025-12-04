de1a06a2d2fe2682e81c8248344bd114b4a92cb7
IrisYoimiya <irischan400@gmail.com>
2025-12-04 20:21:28 +0700

    Fixed Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { ChevronDown } from 'lucide-react';
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
